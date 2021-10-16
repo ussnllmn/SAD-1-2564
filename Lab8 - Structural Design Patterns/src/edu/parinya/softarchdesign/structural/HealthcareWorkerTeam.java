488debb026a596b839921bee9e143e21e8cbe27e
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-10-16 17:40:39 +0700

    Fix HealthcareWorkerTeam

--- Lab8 - Structural Design Patterns/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
+++ Lab8 - Structural Design Patterns/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
@@ -4,13 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HealthcareWorkerTeam implements HealthcareServiceable{
-    private Set<HealthcareServiceable> member = new HashSet<>();
-    public void addMember(HealthcareServiceable newMember){ member.add(newMember); }
-    public void removeMember(HealthcareServiceable delMember){ member.remove(delMember); }
+    private Set<HealthcareServiceable> members = new HashSet<>();
+    public void addMember(HealthcareServiceable newMember){ members.add(newMember); }
+    public void removeMember(HealthcareServiceable delMember){ members.remove(delMember); }
     
     @Override
     public void service() {
-        for(HealthcareServiceable member : member){
+        for(HealthcareServiceable member : members){
             member.service();
         }
     }
@@ -18,7 +18,7 @@ public class HealthcareWorkerTeam implements HealthcareServiceable{
     @Override
     public double getPrice() {
         double totalPrice=0;
-        for(HealthcareServiceable member : member){
+        for(HealthcareServiceable member : members){
             totalPrice += member.getPrice();
         }
         return totalPrice;
