e3e6061720e4c81fa91253a20558f265835cf4ca
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-10-16 17:17:09 +0700

    Edit HealthcareServiceable

--- Lab8 - Structural Design Patterns/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
+++ Lab8 - Structural Design Patterns/src/edu/parinya/softarchdesign/structural/HealthcareWorkerTeam.java
@@ -5,16 +5,22 @@ import java.util.Set;
 
 public class HealthcareWorkerTeam implements HealthcareServiceable{
     private Set<HealthcareServiceable> member = new HashSet<>();
-    public void addMember(HealthcareServiceable newMember){this.member.add(newMember)}
-    public void removeMember(HealthcareServiceable delMember){this.member.remove(delMember)}
+    public void addMember(HealthcareServiceable newMember){ member.add(newMember); }
+    public void removeMember(HealthcareServiceable delMember){ member.remove(delMember); }
     
     @Override
     public void service() {
-
+        for(HealthcareServiceable member : member){
+            member.service();
+        }
     }
 
     @Override
     public double getPrice() {
-        return 0;
+        double totalPrice=0;
+        for(HealthcareServiceable member : member){
+            totalPrice += member.getPrice();
+        }
+        return totalPrice;
     }
 }
