0dd4ac04903eba9f4153f952f7879f7d9c758009
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-10-28 23:33:08 +0700

    Edit main

--- Lab9 - Behavior Design Patterns 1/src/Main.java
+++ Lab9 - Behavior Design Patterns 1/src/Main.java
@@ -1,3 +1,17 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args){
+
+        StringPublisher publisher = new StringPublisher();
+        AlphabetSubscriber alphabetSubscriber = new AlphabetSubscriber();
+        NumberSubscriber numberSubscriber = new NumberSubscriber();
+        SymbolSubscriber symbolSubscriber = new SymbolSubscriber();
+
+        publisher.subscribe(alphabetSubscriber);
+        publisher.subscribe(numberSubscriber);
+        publisher.subscribe(symbolSubscriber);
+
+
+    }
 }
