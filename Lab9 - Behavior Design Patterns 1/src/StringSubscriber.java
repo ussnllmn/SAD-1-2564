4c7fc12c9a4cd9cd4091252bc9e53141db01ced0
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-10-29 21:37:52 +0700

    Add StringSubscriber.java

--- Lab9 - Behavior Design Patterns 1/src/StringSubscriber.java
+++ Lab9 - Behavior Design Patterns 1/src/StringSubscriber.java
@@ -1,14 +1,28 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.Flow;
 
 public abstract class StringSubscriber implements Flow.Subscriber<String> {
     private StringSubscription stringSubscription;
-    private String Data;
+    private String text;
 
-    public void StringSubscriber(String item,String file) throws IOException {
-
+    public void PrintToFile(String text,String file) throws IOException {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                FileWriter myWriter = new FileWriter("filename.txt");
+                myWriter.write(text);
+                myWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
+
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
 
