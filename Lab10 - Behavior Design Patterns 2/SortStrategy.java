9f9ea6f5c7b38f5c5d1e492201c11898a5a02ca7
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-04 20:30:32 +0700

    Change to new project

--- Lab10 - Behavior Design Patterns 2/SortStrategy.java
+++
@@ -1,8 +0,0 @@
-package edu.parinya.softarchdesign.behavior2;
-
-// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
-import java.util.List;
-
-public interface SortStrategy {
-    void customSort(List<Person> people);
-}
